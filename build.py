285ebed3bed53d83051e4d5e11ec4ff5902d95dd
chaowen guo <chaowen.guo1@gmail.com>
2020-10-11 22:21:56 -0500

    Update build.py

--- build.py
+++ build.py
@@ -3,13 +3,10 @@ async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.1/frp_0.34.1_linux_amd64.tar.gz') as response:
             with io.BytesIO(await response.content.read()) as _:
-                #def f(tar):
-                #    for _ in tar.getmembers():
-                #        parts = pathlib.Path(_.name).parts 
-                #        if len(parts) > 2 and parts[1] == 'boost':
-                #            _.name = '/'.join(('include', *itertools.islice(parts, 1, None)))
-                #            yield _
-                with tarfile.open(mode='r:gz', fileobj=_) as tar: print([_.name for _ in tar.getmembers()])#tar.extractall(members=f(tar))
+                def f(tar):
+                    for _ in tar.getmembers():
+                        if pathlib.Path(_.name).name == 'frps': yield _
+                with tarfile.open(mode='r:gz', fileobj=_) as tar: tar.extractall(members=f(tar))
     
 asyncio.run(f())
 
