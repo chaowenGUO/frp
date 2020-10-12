1c061a92bc7ca9c112f581efeba1c2d96d263a78
chaowen guo <chaowen.guo1@gmail.com>
2020-10-11 22:25:54 -0500

    Update build.py

--- build.py
+++ build.py
@@ -13,5 +13,6 @@ asyncio.run(f())
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
+    repository.git.add(A=True)
     repository.index.commit('')#git commit --allow-empty-message -m ''
     repository.remote().push()
