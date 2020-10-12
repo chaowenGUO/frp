f610943a8677600c70a48d28a3fe3f8b743fe5f3
chaowen guo <chaowen.guo1@gmail.com>
2020-10-11 21:58:55 -0500

    Update build.py

--- build.py
+++ build.py
@@ -16,8 +16,5 @@ asyncio.run(f())
 with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
     repository.config_writer().set_value('user', 'name', 'Your Name').release()
     repository.config_writer().set_value('user', 'email', 'you@example.com').release()
-    static = pathlib.Path('/static')
-    repository.git.subtree('add', '--prefix=' + str(static), 'https://github.com/chaowenGUO/aiohttp', 'master', '--squash')
-    for _  in static.iterdir():
-        if _.suffix == '.html' or _.suffix == '.js' or _.suffix == '.sql': shutil.move(str(_), static.parent)
-    shutil.rmtree(static)
+    repository.index.commit('')#git commit --allow-empty-message -m ''
+    repository.remote().push()
