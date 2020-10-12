1299504aee44264dac3265f51dbfa31c59d1f6ef
chaowen guo <chaowen.guo1@gmail.com>
2020-10-11 22:36:15 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, io, tarfile, pathlib, itertools, git, shutil
+import aiohttp, asyncio, io, tarfile, pathlib
 async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.1/frp_0.34.1_linux_amd64.tar.gz') as response:
@@ -11,10 +11,3 @@ async def f():
                 with tarfile.open(mode='r:gz', fileobj=_) as tar: tar.extractall(members=f(tar))
     
 asyncio.run(f())
-
-with git.Repo(pathlib.Path(__file__).resolve().parent) as repository:
-    repository.config_writer().set_value('user', 'name', 'Your Name').release()
-    repository.config_writer().set_value('user', 'email', 'you@example.com').release()
-    repository.git.add(A=True)
-    repository.index.commit('')#git commit --allow-empty-message -m ''
-    repository.remote().push()
