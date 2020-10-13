da2402f859fd4aebf28a31da50b256baf49171eb
chaowen guo <chaowen.guo1@gmail.com>
2020-10-13 16:47:48 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
 from aiohttp import web
 
+async def post(request):
+    print(request.host)
+    return web.Response(text='fuck you')
+
 app = web.Application()
 app.add_routes([web.post('/ajax', post)])
 web.run_app(app, port=os.getenv('PORT'))
