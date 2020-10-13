82dd721305879597aeddc73dc3c931430b607b97
chaowen guo <chaowen.guo1@gmail.com>
2020-10-13 16:43:50 -0500

    Update build.py

--- build.py
+++ build.py
@@ -1,13 +1,5 @@
-import aiohttp, asyncio, io, tarfile, pathlib
-async def f():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.1/frp_0.34.1_linux_amd64.tar.gz') as response:
-            with io.BytesIO(await response.content.read()) as _:
-                def f(tar):
-                    for _ in tar.getmembers():
-                        if pathlib.Path(_.name).name == 'frps':
-                            _.name = 'frps'
-                            yield _
-                with tarfile.open(mode='r:gz', fileobj=_) as tar: tar.extractall(members=f(tar))
-    
-asyncio.run(f())
+from aiohttp import web
+
+app = web.Application()
+app.add_routes([web.post('/ajax', post)])
+web.run_app(app, port=os.getenv('PORT'))
