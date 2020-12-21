7de82e57a8662dc4bf57b30c328bd2749fa0e84c
chaowen guo <chaowen.guo1@gmail.com>
2020-12-21 01:35:58 -0600

    Update frpc.py

--- frpc.py
+++ frpc.py
@@ -6,5 +6,13 @@ async def f():
                 with tarfile.open(mode='r:gz', fileobj=_) as tar: 
                     tar.extract('frp_0.34.3_linux_amd64/systemd/frpc.service', '/lib/systemd/system')
                     tar.extract('frp_0.34.3_linux_amd64/frpc', '/usr/bin')
-        pathlib.Path('/etc/frp/frpc.ini').write_text(
+    pathlib.Path('/etc/frp/frpc.ini').write_text('''[common]
+server_addr = [2a02:180:6:1::3142]
+[web]
+type = tcp
+local_port = 8080
+remote_port = 6000''')
+    await asyncio.create_subprocess_exec('systemctl', 'daemon-reload')
+    await asyncio.create_subprocess_exec('systemctl', 'enable', 'frpc')   
+
 asyncio.run(f())
