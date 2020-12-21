790f29c2073f0e060da0a31fe559590cc9362dee
chaowen guo <chaowen.guo1@gmail.com>
2020-12-20 22:30:49 -0600

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-import os
-
-async def post(request):
-    return web.Response(text=str(request.headers))
-
-app = web.Application()
-app.add_routes([web.post('/', post)])
-web.run_app(app, port=os.getenv('PORT'))
