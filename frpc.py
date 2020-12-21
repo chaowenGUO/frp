3080844a539bf9330a3d8cf3a5c61bee1f0b1004
chaowen guo <chaowen.guo1@gmail.com>
2020-12-21 01:27:24 -0600

    Update frpc.py

--- frpc.py
+++ frpc.py
@@ -3,11 +3,8 @@ async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz') as response:
             with io.BytesIO(await response.content.read()) as _:
-                def f(tar):
-                    for _ in tar.getmembers():
-                        parts = pathlib.Path(_.name).parts 
-                        if len(parts) > 2 and parts[1] == 'boost':
-                            _.name = '/'.join(('include', *itertools.islice(parts, 1, None)))
-                            yield _
-                with tarfile.open(mode='r:gz', fileobj=_) as tar: tar.extractall(members=f(tar))
+                with tarfile.open(mode='r:gz', fileobj=_) as tar: 
+                    tar.extract('frp_0.34.3_linux_amd64/systemd/frpc.service', '/lib/systemd/system')
+                    tar.extract('frp_0.34.3_linux_amd64/frpc', '/usr/bin')
+        pathlib.Path('/etc/frp/frpc.ini').write_text(
 asyncio.run(f())
