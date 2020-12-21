b960cf5a4f2936980a0584cd42737858c9f7e3da
chaowen guo <chaowen.guo1@gmail.com>
2020-12-21 03:15:15 -0600

    Update frpc.py

--- frpc.py
+++ frpc.py
@@ -5,8 +5,8 @@ async def f():
         async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz') as response:
             with io.BytesIO(await response.content.read()) as _:
                 with tarfile.open(mode='r:gz', fileobj=_) as tar:
-                    for _ in tar: _.name = _.name.split('/', 1)[-1]
-                    tar.extract('systemd/frpc.service', '/lib/systemd/system')
+                    for _ in tar: _.name = pathlib.Path(_.name).name
+                    tar.extract('frpc.service', '/lib/systemd/system')
                     tar.extract('frpc', '/usr/bin')
     pathlib.Path('/etc/frp/frpc.ini').write_text('''[common]
 server_addr = [2a02:180:6:1::3142]
