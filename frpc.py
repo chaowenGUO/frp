41086ae59b1bacf365f9925b4b0ab5cdba0e24d5
chaowen guo <chaowen.guo1@gmail.com>
2020-12-21 02:33:30 -0600

    Update frpc.py

--- frpc.py
+++ frpc.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, io, tarfile
+import aiohttp, asyncio, io, tarfile, pathlib
 async def f():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz') as response:
